2ac01950d58a4a9fc6d583285f6e2060dec32043
mirai <yasuo.hayashibara@it-chiba.ac.jp>
2017-12-14 09:29:39 +0900

    dummy_robot.py

--- scripts/dummy_robot.py
+++ scripts/dummy_robot.py
@@ -9,24 +9,37 @@ from cv_bridge import CvBridge, CvBridgeError
 from learning_human_interaction.msg import Interaction_robot_status
 from std_msgs.msg import Float32, Int8
 import numpy as np
+import math
+import read_key
 
 class Environment:
     def __init__(self):
+        self.L = 0.3
+        self.target_distance = 0.45
+        target_angle = math.acos(self.target_distance/(2.0*self.L))
         self.robot_x = 0
-        self.robot_xd = 0
-        self.robot_xdd = 0
         self.human_x = 0.4
-        self.angle_errors = [0, 0, 0]
-        self.target_angles = [0, 0, 0]
-
+        current_angle = math.acos((self.human_x-self.robot_x)/(2.0*self.L))
+        self.max_error_x = 0.1
+        self.angle_errors = np.array([0, 0, 0])
+        self.target_angles = np.array([-target_angle, 2.0*target_angle, -target_angle])
+        self.current_angles = np.array([-current_angle, 2.0*current_angle, -current_angle])
         self.WIDTH = 640
         self.HEIGHT = 480
         self.cv_image = np.zeros((self.HEIGHT, self.WIDTH, 3), np.uint8)
 
-    def calculate(self):
-        L1 = 0.5
-        L2 = 0.5
-
+    def calculate(self, move_x):
+        self.human_x += move_x
+        distance = self.human_x - self.robot_x
+        error = self.target_distance - distance
+        error = min([max([error, -self.max_error_x]), self.max_error_x])
+        distance = self.target_distance - error
+        self.human_x = self.robot_x + distance
+        target_angle = math.acos(self.target_distance / (2.0 * self.L))
+        self.target_angles = np.array([-target_angle, 2.0 * target_angle, -target_angle])
+        current_angle = math.acos(distance / (2.0 * self.L))
+        self.current_angles = np.array([-current_angle, 2.0 * current_angle, -current_angle])
+        self.angle_errors = self.target_angles - self.current_angles
 
     def draw(self):
         CX = self.WIDTH / 2
@@ -35,11 +48,22 @@ class Environment:
 
         self.cv_image.fill(255)
         cv2.line(self.cv_image, (0, CY), (self.WIDTH, CY), (0, 0, 0), 3)
-        cv2.rectangle(self.cv_image, (self.robot_x * R + CX, CY), (self.robot_x + CX - 100, CY - 200), (255, 0, 0), 3)
-        cv2.line(self.cv_image, (int(self.human_x * R + CX), CY), (int(self.human_x * R + CX), CY - 200), (0, 0, 255), 3)
+        RX = self.robot_x * R + CX
+        cv2.rectangle(self.cv_image, (RX, CY), (RX - 100, CY - 200), (255, 0, 0), 3)
+        HX = int(self.human_x * R + CX)
+        cv2.line(self.cv_image, (HX, CY), (HX, CY - 200), (0, 0, 0), 3)
+        AY = CY - 150
+        KX = int((self.human_x + self.robot_x) / 2.0 * R + CX)
+        KY = int(math.sqrt(self.L ** 2 - ((self.human_x - self.robot_x) / 2.0) ** 2) * R + AY)
+        cv2.line(self.cv_image, (RX, AY), (KX, KY), (0, 0, 0), 2)
+        cv2.line(self.cv_image, (KX, KY), (HX, AY), (0, 0, 0), 2)
+        TX = int((self.robot_x + self.target_distance / 2.0) * R + CX)
+        TY = int(math.sqrt(self.L ** 2 - (self.target_distance / 2.0) ** 2) * R + AY)
+        THX = int((self.robot_x + self.target_distance) * R + CX)
+        cv2.line(self.cv_image, (RX, AY), (TX, TY), (0, 0, 255), 2)
+        cv2.line(self.cv_image, (TX, TY), (THX, AY), (0, 0, 255), 2)
         cv2.imshow("robot", self.cv_image)
         cv2.waitKey(1)
-
 
 class dummy_robot:
     def __init__(self):
@@ -65,6 +89,16 @@ class dummy_robot:
         self.prev_count = -1
 
     def callback_publish_status_timer(self, data):
+        c = ''
+        if read_key.key_pressed():
+            c = read_key.read_key()
+        if c == 'x':
+            move_x = 0.01
+        elif c == 'z':
+            move_x = -0.01
+        else:
+            move_x = 0
+        self.env.calculate(move_x)
         self.env.draw()
         self.interaction_robot_status_pub.publish(self.interaction_robot_status)
 
